060412e53ca59398ff4d94c14289fc494c40fdb4
tatusmatrixAB <tatusmatrix@gmail.com>
2023-04-29 19:04:50 +0300

    20230429_19_04_Class_06_03

--- 06_nodarbiiba/js/if_else.js
+++ 06_nodarbiiba/js/if_else.js
@@ -1,4 +1,4 @@
-const hour = new Date().getHours(); 
+const hour = new Date().getHours();
 let greeting = "Good ";
 
 /*
@@ -11,17 +11,20 @@ if (hour < 18) {
 console.log(greeting);
 
 if (hour < 18) {
-    //greeting = "Good";
-    //console.log(greeting);
-    greeting = greeting + "day";
-    console.log(greeting);}
-   else {
-    //greeting = "Good";
-    greeting = greeting + "evening";
-  }
-
+  //greeting = "Good";
+  //console.log(greeting);
+  greeting = greeting + "day";
   console.log(greeting);
-  
+} else {
+  //greeting = "Good";
+  greeting = greeting + "evening";
+  if (hour > 22) {
+    greeting = greeting + " (it actually is late evening :-))";
+  } else {
+    greeting = greeting + " (it is not late evening yet :-))";
+  }
+}
 
+console.log(greeting);
 
 document.getElementById("demo_if_else").innerHTML = greeting;
