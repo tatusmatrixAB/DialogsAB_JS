32055878116a80bbf11308365ab2d9f60a9f272e
tatusmatrixAB <tatusmatrix@gmail.com>
2023-05-11 20:39:51 +0300

    Class_07_20230511_20_39_03

--- 07_nodarbiiba/js/functions.js
+++ 07_nodarbiiba/js/functions.js
@@ -1,6 +1,7 @@
 function counter(paragraph_name, end_value, step_value){
   let text = "";
   let i = 1;
+
   while (i <= end_value) {
     //console.log("i: " + i);
     //console.log("i%2: " + (i % 2));
@@ -10,11 +11,27 @@ function counter(paragraph_name, end_value, step_value){
     i++;
   }
   document.getElementById(paragraph_name).innerHTML = text;
+  // return i;
+  return [i, i*i];
 }
+// u(t) = t*t;
 
-counter("demo_function_1",20,2);
+//var cits_i;
+//cits_i = counter("demo_function_1",20,2);
+//console.log("Mainīgā i vērtība pēc funkcijas darbināšanas: " + cits_i);
 
-counter("demo_function_2",40,3);
+//cits_i = counter("demo_function_2",40,3);
+//console.log("Mainīgā i vērtība pēc funkcijas darbināšanas: " + cits_i);
+
+var funkcijas_darbiibas_rezultaati;
+funkcijas_darbiibas_rezultaati = counter("demo_function_1",20,2);
+console.log("Pirmā rezultāta vērtība pēc funkcijas darbināšanas: " + funkcijas_darbiibas_rezultaati[0]);
+console.log("Otrā rezultāta vērtība pēc funkcijas darbināšanas: " + funkcijas_darbiibas_rezultaati[1]);
+
+funkcijas_darbiibas_rezultaati = counter("demo_function_2",40,3);
+console.log("Pirmā rezultāta vērtība pēc funkcijas darbināšanas: " + funkcijas_darbiibas_rezultaati[0]);
+console.log("Otrā rezultāta vērtība pēc funkcijas darbināšanas: " + funkcijas_darbiibas_rezultaati[1]);
+
 
 /*
 text = "";
