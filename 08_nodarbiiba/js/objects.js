a307e66ebabc4664164a6dd53f39b82e05be4a69
tatusmatrixAB <tatusmatrix@gmail.com>
2023-05-13 19:37:20 +0300

    Class_08_20230513_19_37_01

---
+++ 08_nodarbiiba/js/objects.js
@@ -0,0 +1,5 @@
+const discrete_variable = 4;
+const array_example = ['a','b'];
+const function_example = function name_for_internal_use(x){return x*x;};
+
+const car = {type:"Fiat", model:"500", color:"white"};
