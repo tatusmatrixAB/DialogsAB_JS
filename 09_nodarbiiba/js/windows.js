8f92b719e1bf4451eebd36fbe1a3a55ce0d9fe72
tatusmatrixAB <tatusmatrix@gmail.com>
2023-05-20 18:53:08 +0300

    Class_09_20230520_18_52_02

--- 09_nodarbiiba/js/windows.js
+++ 09_nodarbiiba/js/windows.js
@@ -12,24 +12,32 @@ showCounter();
 console.log(window.counter);
 window.showCounter();
 
-console.log('window.innerWidth: ' + window.innerWidth);
-console.log('document.documentElement.clientWidth: ' + document.documentElement.clientWidth);
-console.log('document.body.clientWidth: ' + document.body.clientWidth);
-const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+console.log("window.innerWidth: " + window.innerWidth);
+console.log(
+  "document.documentElement.clientWidth: " +
+    document.documentElement.clientWidth
+);
+console.log("document.body.clientWidth: " + document.body.clientWidth);
+const width =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
 console.log(`window.outerWidth: ${window.outerWidth}`);
 
-const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight; 
+const height =
+  window.innerHeight ||
+  document.documentElement.clientHeight ||
+  document.body.clientHeight;
 
-let features = 'height=600,width=800',
-    url = 'https://www.javascripttutorial.net/javascript-bom/javascript-window/';
+let features = "height=600,width=800",
+  //url = 'https://www.javascripttutorial.net/javascript-bom/javascript-window/';
+  url = "http://127.0.0.1:5500";
 
-/*
-setTimeout(() => {
-    //window.open('https://www.javascripttutorial.net', 'about')
-    //window.open('http://127.0.0.1:5500', 'about')
-    let jsWindow = window.open(url, 'about', features);
-}, 20000);
-*/
+//setTimeout(() => {
+//window.open('https://www.javascripttutorial.net', 'about')
+//window.open('http://127.0.0.1:5500', 'about')
+//let jsWindow = window.open(url, 'about', features);
+//}, 20000);
 
 /*
 setTimeout(() => {
@@ -66,18 +74,44 @@ var timeoutID_1;
 var timeoutID_2;
 
 function showAlert() {
-    timeoutID_1 = setTimeout(alert, 3000, 'setTimeout Demo 1!');
-    timeoutID_2 = setTimeout(alert, 5000, 'setTimeout Demo 2!');
+  timeoutID_1 = setTimeout(alert, 3000, "setTimeout Demo 1!");
+  timeoutID_2 = setTimeout(alert, 5000, "setTimeout Demo 2!");
 }
 
 function cancelAlert_1() {
-    console.log(timeoutID_1);
-    console.log(clearTimeout(timeoutID_1));
-    //console.log(timeoutID);
-    //console.log(clearTimeout(timeoutID));
+  console.log(timeoutID_1);
+  console.log(clearTimeout(timeoutID_1));
+  //console.log(timeoutID);
+  //console.log(clearTimeout(timeoutID));
 }
 
 function cancelAlert_2() {
-    console.log(timeoutID_2);
-    console.log(clearTimeout(timeoutID_2));
+  console.log(timeoutID_2);
+  console.log(clearTimeout(timeoutID_2));
+}
+
+var intervalID_1;
+//intervalID_1 = setInterval(console.log, 1000,'Viena sekunde no mūsu dzīves ir pagājusi ...');
+console.log(intervalID_1);
+
+let intervalID;
+
+function toggleColor() {
+  let e = document.getElementById("flashtext");
+  console.log(e);
+  console.log(e.style);
+  console.log(e.style.color);
+  console.log(e.style.color == "red");
+  console.log(e.style.color == "red" ? "blue" : "red");
+  e.style.color = e.style.color == "red" ? "blue" : "red";
+  console.log(e.style.color);
+  console.log("\n");
+}
+
+function stop() {
+  clearInterval(intervalID);
+}
+
+function start() {
+  intervalID = setInterval(toggleColor, 1000);
 }
