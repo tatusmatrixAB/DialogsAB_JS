7e9bb08abae131b98f8bbab8859ff039488d5aef
tatusmatrixAB <tatusmatrix@gmail.com>
2023-05-28 19:53:00 +0300

    Class_11_20230528_19_52_03

--- 11_nodarbiiba/js/cookies_.js
+++ 11_nodarbiiba/js/cookies_.js
@@ -6,15 +6,14 @@
 3. Cookie mainīšana
 4. Cookie "dzīves laika" uzdošana (ja laiks nav uzdots - Session)
 5. Cookie izmantošana pēc pārlūka aizvēršanas, atvēršana
--------
-6. Cookie kodēšana
-7. Cookie dzēšana
+6. Cookie kodēšana (bet ne šifrēšana; piemēram, ā -> %C4%81)
+7. Cookie dzēšana (iztecējušā laika uzdošana)
 */
 
 var str = document.cookie;
 console.log("cookie vērtība: " + str);
 
-display("username_1", "admin_1", 30);
+display("username_1", "administrācija_1", 30);
 display("username_2", "admin_2");
 
 //document.cookie = "username_1=admin_1";
@@ -34,7 +33,8 @@ function display(cookie_name, cookie_value, cookie_expire_time_sec) {
   if (cookie_expire_time_sec != undefined) {
     var expireTime = time + 1000 * cookie_expire_time_sec;
     now.setTime(expireTime);
-    document.cookie = cookie_name + "=" + cookie_value + ";expires=" + now.toUTCString() + ";path=/";
+    //document.cookie = cookie_name + "=" + cookie_value + ";expires=" + now.toUTCString() + ";path=/";
+    document.cookie = `${encodeURIComponent(cookie_name)}=${encodeURIComponent(cookie_value)};expires=${now.toUTCString()};path=/`;
   } else {
     document.cookie = cookie_name + "=" + cookie_value + ";path=/";
   }
