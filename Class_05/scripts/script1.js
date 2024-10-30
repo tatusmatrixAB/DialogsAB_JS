05401ad4883013ead765ddaca69e60540009b197
tatusmatrixAB <tatusmatrix@gmail.com>
2024-10-30 19:17:44 +0200

    Class_05d

--- Class_05/scripts/script1.js
+++ Class_05/scripts/script1.js
@@ -2,8 +2,15 @@ const cars = ["Saab", "Volvo", "BMW"];
 document.getElementById("array_initial").innerHTML = `${cars} masīva izmērs - ${cars.length} elementi`;
 
 // cars[10] = "Renault"; - tā nedarām!
-var N = cars.push("VW");
+var N = cars.push("VW"); // var pieveinot vairākus elementus
 document.getElementById("array_push").innerHTML = `${cars} masīva izmērs - ${N} elementi`;
+
+N = cars.unshift("Mercedes"); // var pieveinot vairākus elementus
+document.getElementById("array_unshift").innerHTML = `${cars} masīva izmērs - ${N} elementi`;
+
+var cars_spliced = cars.splice(1,1,"RAF","Dodge");
+document.getElementById("array_splice_with_add").innerHTML = `${cars} masīva izmērs - ${cars.length} elementi (izdzēstie elementi - ${cars_spliced})`;
+
 
 // delete nedarām
 //delete cars[0];
@@ -15,4 +22,6 @@ document.getElementById("array_pop").innerHTML = `${cars} masīva izmērs - ${ca
 var car_first = cars.shift();
 document.getElementById("array_shift").innerHTML = `${cars} masīva izmērs - ${cars.length} elementi (izdzēstais elements - ${car_first})`;
 
+cars_spliced = cars.splice(1,2);
+document.getElementById("array_splice_without_add").innerHTML = `${cars} masīva izmērs - ${cars.length} elementi (izdzēstie elementi - ${cars_spliced})`;
 
