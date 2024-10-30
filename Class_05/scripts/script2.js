49e3edee5e07e511361266663ad03a1792d4d8c0
tatusmatrixAB <tatusmatrix@gmail.com>
2024-10-30 21:17:16 +0200

    Class_05f

--- Class_05/scripts/script2.js
+++ Class_05/scripts/script2.js
@@ -2,9 +2,14 @@
 
 const numbers = [4, 9, 16, 25, 29];
 let first = numbers.find(myFunction);
+//let first = numbers.findIndex(myFunction);
+//let first = numbers.findLast(myFunction);
+//let first = numbers.findLastIndex(myFunction);
 
-document.getElementById("array_find").innerHTML =
-  "First number over 18 is " + first;
+document.getElementById("array_find").innerHTML = "First number over 18 is " + first;
+//document.getElementById("array_find").innerHTML = "Index of first number over 18 is " + first;
+//document.getElementById("array_find").innerHTML = "First (from end) number over 18 is " + first;
+//document.getElementById("array_find").innerHTML = "Index of first (from end) number over 18 is " + first;
 
 function myFunction(value, index, array) {
   console.log(value, index, value > 18, array);
---
+++ Class_05/scripts/script3.js
@@ -0,0 +1,12 @@
+N = numbers.length;
+var i = 0;
+
+console.log(numbers);
+console.log(i < N);
+
+while (i < N) {
+  console.log(i < N);
+  console.log(i, numbers[i]);
+  i++;
+  console.log(i < N);
+}
