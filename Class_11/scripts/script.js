55c118d99cd0996f73775de0acad03d5385261ae
tatusmatrixAB <tatusmatrix@gmail.com>
2024-11-27 19:36:30 +0200

    Class_11d

--- Class_11/scripts/script.js
+++ Class_11/scripts/script.js
@@ -39,11 +39,15 @@ console.log("Pārlūka loga ārējās daļas augstums - " + outer_height);
 
 //let url = 'http://127.0.0.1:5500/Class_07/';
 //let jsWindow = window.open(url,'Class_07');
+//pārlūka "fokusa" pārslēgšana uz tikko atvērto tabu/logu
+//window.focus();
 
 // drošības pēc lokālu failu aplūkošana ir bloķēta
 //let url = 'file:///C:/Users/Tatjana/Downloads/DialogsAB/DialogsAB_JS_V/DialogsAB_JS/Class_10/index.html';
 //let jsWindow = window.open(url,'local');
 
+
+/*
 let jsWindow = window.open(
   //"https://rtu.lv",
   "http://127.0.0.1:5500/Class_08/index.html",
@@ -63,3 +67,22 @@ setTimeout(() => {
 setTimeout(() => {
     jsWindow.close();
 }, 10000);
+*/
+
+// https://www.javascripttutorial.net/javascript-bom/javascript-alert/
+//alert("Cienījamais lietotāj, tulīt mēs tevi infoemēsim par ...");
+
+// https://www.javascripttutorial.net/javascript-bom/javascript-confirm/
+//let result = window.confirm("Jā vai nē?");
+//let message = result ? "Jūs nospiedāt OK pogu" : 'Jūs nospiedāt Cancel pogu';
+//alert(message);
+
+// https://www.javascripttutorial.net/javascript-bom/javascript-prompt/
+let lang = prompt('What is your favorite programming language?');
+
+let feedback = lang.toLowerCase() === 'javascript' ? `It's great!` :
+    `It's ${lang}`;
+
+alert(feedback);
+
+
