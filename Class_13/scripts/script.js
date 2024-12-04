9f4729aaeefff23dda313d04b5cecef0b368aa2f
tatusmatrixAB <tatusmatrix@gmail.com>
2024-12-04 18:57:40 +0200

    Class_13c

--- Class_13/scripts/script.js
+++ Class_13/scripts/script.js
@@ -3,14 +3,28 @@
 
 let btn = document.getElementById('btnCount');
 
+// var headings;
 
 btn.addEventListener('click', () => {
     let headings = document.getElementsByTagName('h2');
     alert(`The number of H2 tags (counted by getElementsByTagName): ${headings.length}`);
 });
 
+/*
 btn.addEventListener('dblclick', () => {
     let headings = document.querySelectorAll('h2');
     alert(`The number of H2 tags (counted by querySelectorAll): ${headings.length}`);
 });
+*/
 
+// -----------------------------------------------------------------------------------------
+// https://www.javascripttutorial.net/javascript-dom/javascript-getelementsbyclassname/
+// https://www.javascripttutorial.net/javascript-dom/javascript-queryselector/
+
+let menu = document.getElementById('menu');
+// let items = menu.getElementsByClassName('case1');
+ let items = menu.querySelectorAll('.case1');
+console.log(items);
+
+let data = [].map.call(items, (item) => item.textContent);
+console.log(data);
