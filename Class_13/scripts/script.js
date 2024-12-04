ad574325bf7abfd10284c56a8c8d07ff20e3a9fc
tatusmatrixAB <tatusmatrix@gmail.com>
2024-12-04 18:29:04 +0200

    Class_13b

--- Class_13/scripts/script.js
+++ Class_13/scripts/script.js
@@ -3,14 +3,14 @@
 
 let btn = document.getElementById('btnCount');
 
-/*
+
 btn.addEventListener('click', () => {
     let headings = document.getElementsByTagName('h2');
-    alert(`The number of H2 tags: ${headings.length}`);
+    alert(`The number of H2 tags (counted by getElementsByTagName): ${headings.length}`);
 });
-*/
-btn.addEventListener('click', () => {
+
+btn.addEventListener('dblclick', () => {
     let headings = document.querySelectorAll('h2');
-    alert(`The number of H2 tags: ${headings.length}`);
+    alert(`The number of H2 tags (counted by querySelectorAll): ${headings.length}`);
 });
 
